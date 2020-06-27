8cd515ff79615327f73ba92eb310091239074726
Geoff <gwebb762@gmail.com>
2020-06-26 23:38:50 -0500

    Resume page grid layout

--- src/pages/work/Work.js
+++ src/pages/work/Work.js
@@ -12,26 +12,6 @@ const Work = () => {
 
   return (
     <div>
-      <h1>Work</h1>
-
-      {/* <Container>
-        <Row className="justify-content-md-center">
-          <Col xs lg="2">
-            1 of 3
-          </Col>
-          <Col md="auto">Variable width content</Col>
-          <Col xs lg="2">
-            3 of 3
-          </Col>
-        </Row>
-        <Row>
-          <Col>1 of 3</Col>
-          <Col md="auto">Variable width content</Col>
-          <Col xs lg="2">
-            3 of 3
-          </Col>
-        </Row>
-      </Container> */}
       <div className="work-wrapper">
         <Resume resumes={user.resumes} />
       </div>
--- src/pages/work/resume/Resume.js
+++ src/pages/work/resume/Resume.js
@@ -8,32 +8,27 @@ const Resume = ({ resumes }) => {
   const [resume, setResume] = useState(resumes[0]);
 
   return (
-    <div className="resume-wrapper">
-      <div className="user-panel"></div>
-      <div className="resume-panel">
-        <Container>
-          <Col>
-            <img src="" />
-            <h2>Summary</h2>
-            <div>Education Box</div>
-          </Col>
-          <Col>
-            <Row className="skill-panel justify-content-between">
-              {resume.skills.map((skill) => (
-                <Col key={skill.name} md>
-                  <span>{skill.name}</span>
-                </Col>
-              ))}
-            </Row>
-            <Row className="job-panel">
-              <h2>Job 1</h2>
-              <h2>Job 2</h2>
-              <h2>Job 3</h2>
-            </Row>
-          </Col>
-        </Container>
-      </div>
-    </div>
+    <Container className="resume-panel">
+      <Col sm="1" className="user-panel">
+        <img src="" />
+        <h2>Summary</h2>
+        <div>Education Box</div>
+      </Col>
+      <Col sm="auto" className="work-panel">
+        <Row className="skill-panel justify-content-between">
+          {resume.skills.map((skill) => (
+            <Col key={skill.name} md>
+              <span>{skill.name}</span>
+            </Col>
+          ))}
+        </Row>
+        <Col className="job-panel">
+          <h2>Job 1</h2>
+          <h2>Job 2</h2>
+          <h2>Job 3</h2>
+        </Col>
+      </Col>
+    </Container>
   );
 };
 

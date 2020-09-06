de3e14d465d584f13dd5f9910ea19363d75314f6
Geoff <gwebb762@gmail.com>
2020-09-06 18:29:29 -0500

    PWC-000 Build

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,7 +1,19 @@
 import React from "react";
 
+import ProfilePhoto from "../../../assets/gwebb_profile.jpg";
+import Logo from "../../../assets/gwebb_logo.png";
+
+import "./Home.sass";
+
 const Home = () => (
-  <div>
+  <div className="home-wrapper mh-100 d-flex flex-column justify-content-center">
+    {/* <div className="my-4 mx-16">
+      <img className="home-profile-photo rounded" src={ProfilePhoto} />
+    </div> */}
+    <div className="my-4 mx-4">
+      <img className="mb-4 home-logo" src={Logo} />
+      <h3 className="text-white">Visually minded software developer with a passsion for solving puzzles</h3>
+    </div>
   </div>
 );
 

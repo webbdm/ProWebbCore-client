494b3bb2c25921a9db4027fec7bbb4c274690f70
Geoff <gwebb762@gmail.com>
2020-09-13 12:08:39 -0500

    PWC-000 Make sidebar position sticky

--- src/global/Sidebar.js
+++ src/global/Sidebar.js
@@ -7,24 +7,24 @@ import GitHub from "../../assets/github_logo.png";
 import "./Sidebar.css";
 
 const Sidebar = () => (
-  <div className="sidebar hidden lg:flex xl:flex h-auto bg-panel w-56 py-2 flex-col justify-start items-center">
-    <div>
+  <div className="sidebar hidden lg:flex xl:flex h-auto bg-panel w-56 flex-col justify-start items-center">
+    <div className="sticky top-0 flex flex-col items-center">
       <Link to="/">
         <div className="rounded border-2 border-accent px-2 mt-4 text-center">
           <span className="text-white inline-block text-4xl m-1">W</span>
         </div>
       </Link>
-    </div>
-    <div className="my-2">
-      <a
-        className="block my-8"
-        href="https://www.linkedin.com/in/geoff-webb-85637586"
-      >
-        <img className="icon" src={LinkedIn} />
-      </a>
-      <a className="block my-8" href="https://github.com/webbdm">
-        <img className="icon" src={GitHub} />
-      </a>
+      <div className="my-2">
+        <a
+          className="block my-8"
+          href="https://www.linkedin.com/in/geoff-webb-85637586"
+        >
+          <img className="icon" src={LinkedIn} />
+        </a>
+        <a className="block my-8" href="https://github.com/webbdm">
+          <img className="icon" src={GitHub} />
+        </a>
+      </div>
     </div>
   </div>
 );

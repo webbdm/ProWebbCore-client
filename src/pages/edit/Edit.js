68a6a4c808189543d25bc0ca213b07180af65e77
Geoff <gwebb762@gmail.com>
2020-10-20 13:20:12 -0500

    PWC-13 Buttons

--- src/pages/edit/Edit.js
+++ src/pages/edit/Edit.js
@@ -3,7 +3,7 @@ import { UserContext } from "../../Store";
 
 import ProfilePhoto from "../../../assets/gwebb_profile.jpg";
 
-const EditProject = ({ project }) => {
+const EditProject = ({ project, update }) => {
   const [name, setName] = useState(project.name);
   const [description, setDescription] = useState(project.description);
 
@@ -16,11 +16,16 @@ const EditProject = ({ project }) => {
         src={`${process.env.BUCKET_URL}/${image}`}
       />
       <div className="flex flex-col h-auto w-full sm:mt-2">
-        <input
-          className="bg-panel outline-none text-white rounded-t p-2 border-accent border-b"
-          value={name}
-          onChange={(e) => setName[e.target.value]}
-        />
+        <div className="flex flex-row justify-between bg-panel rounded-t p-2 border-accent border-b w-100">
+          <input
+            className="outline-none text-white bg-panel"
+            value={name}
+            onChange={(e) => setName[e.target.value]}
+          />
+          <div className="bg-panel rounded px-4 py-1 text-white cursor-pointer hover:font-semibold">
+            Save
+          </div>
+        </div>
         <textarea
           className="bg-panel text-white h-32 w-100 outline-none rounded-b resize-none p-2"
           value={description}
@@ -37,6 +42,12 @@ const Edit = () => {
   if (!user) return null;
   return (
     <div className="h-full w-100 flex flex-col m-4">
+      <div className="flex flex-row justify-between items-center border-accent border-b m-4 ml-0 pb-2">
+        <h1 className="text-white text-2xl m-0">Bio</h1>
+        <div className="bg-panel rounded px-4 py-1 text-white cursor-pointer hover:border-accent hover:border">
+          Save
+        </div>
+      </div>
       <div className="flex flex-row flex-wrap justify-start mb-2">
         <img
           className="lg:h-64 sm:h-56 sm:w-auto sm:h-auto md:w-auto rounded-t border-accent border-b-4"
@@ -47,13 +58,15 @@ const Edit = () => {
           className="bg-panel p-2 flex-grow text-white lg:mx-4 resize-none rounded outline-none"
         ></textarea>
       </div>
-      <div className="flex flex-row">
-        <div></div>
-        <div className="flex flex-col w-full">
-          {user.projects.map((project) => (
-            <EditProject project={project} />
-          ))}
-        </div>
+
+      <div className="flex flex-row justify-between items-center border-accent border-b m-4 ml-0 pb-2">
+        <h1 className="text-white text-2xl m-0">Projects</h1>
+      </div>
+
+      <div className="flex flex-col w-full">
+        {user.projects.map((project) => (
+          <EditProject project={project} />
+        ))}
       </div>
     </div>
   );
